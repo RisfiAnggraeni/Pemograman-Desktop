fdcbe8c8db4cbd2b00f951ce55a847d61493a893
Sigityudhaa <sigityudhanto57@gmail.com>
2025-03-07 11:21:49 +0700

    TUGAS 2 PEMOGRAMAN DESKTOP

--- Form Mahasiswaa/Form1.Designer.vb
+++ Form Mahasiswaa/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Form2
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -31,6 +31,10 @@ Partial Class Form1
         Me.cbfakultas = New System.Windows.Forms.ComboBox()
         Me.cbjurusan = New System.Windows.Forms.ComboBox()
         Me.btnprocess = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.rbl = New System.Windows.Forms.RadioButton()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.rbp = New System.Windows.Forms.RadioButton()
         Me.SuspendLayout()
         '
         'Label1
@@ -54,7 +58,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(28, 122)
+        Me.Label3.Location = New System.Drawing.Point(28, 147)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(62, 13)
         Me.Label3.TabIndex = 2
@@ -87,7 +91,7 @@ Partial Class Form1
         '
         Me.cbfakultas.FormattingEnabled = True
         Me.cbfakultas.Items.AddRange(New Object() {"FMIPA", "FKIP", "FEB"})
-        Me.cbfakultas.Location = New System.Drawing.Point(138, 122)
+        Me.cbfakultas.Location = New System.Drawing.Point(138, 139)
         Me.cbfakultas.Name = "cbfakultas"
         Me.cbfakultas.Size = New System.Drawing.Size(121, 21)
         Me.cbfakultas.TabIndex = 6
@@ -110,11 +114,54 @@ Partial Class Form1
         Me.btnprocess.Text = "Process"
         Me.btnprocess.UseVisualStyleBackColor = True
         '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(28, 110)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(71, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Jenis Kelamin"
+        '
+        'rbl
+        '
+        Me.rbl.AutoSize = True
+        Me.rbl.Location = New System.Drawing.Point(138, 106)
+        Me.rbl.Name = "rbl"
+        Me.rbl.Size = New System.Drawing.Size(64, 17)
+        Me.rbl.TabIndex = 10
+        Me.rbl.TabStop = True
+        Me.rbl.Text = "Laki laki"
+        Me.rbl.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(191, 124)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(0, 13)
+        Me.Label6.TabIndex = 11
+        '
+        'rbp
+        '
+        Me.rbp.AutoSize = True
+        Me.rbp.Location = New System.Drawing.Point(208, 108)
+        Me.rbp.Name = "rbp"
+        Me.rbp.Size = New System.Drawing.Size(79, 17)
+        Me.rbp.TabIndex = 12
+        Me.rbp.TabStop = True
+        Me.rbp.Text = "Perempuan"
+        Me.rbp.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(444, 261)
+        Me.Controls.Add(Me.rbp)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.rbl)
+        Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.btnprocess)
         Me.Controls.Add(Me.cbjurusan)
         Me.Controls.Add(Me.cbfakultas)
@@ -139,5 +186,9 @@ Partial Class Form1
     Friend WithEvents cbfakultas As System.Windows.Forms.ComboBox
     Friend WithEvents cbjurusan As System.Windows.Forms.ComboBox
     Friend WithEvents btnprocess As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents rbl As System.Windows.Forms.RadioButton
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents rbp As System.Windows.Forms.RadioButton
 
 End Class
